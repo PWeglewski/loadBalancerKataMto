be053e50b16cef17425b0bafbc76538537724652
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-06-23 00:47:06 +0200

    Added vm counter test

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,19 +3,23 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
-	public static final double MAXIMUM_LOAD = 100.0d;
-	public double currentLoadPecentage;
-	public int capacity;
+    public static final double MAXIMUM_LOAD = 100.0d;
+    public double currentLoadPecentage;
+    public int capacity;
 
-	public Server(int capacity) {
-		this.capacity = capacity;
-	}
+    public Server(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public boolean contains(Vm vm) {
-		return true;
-	}
+    public boolean contains(Vm vm) {
+        return true;
+    }
 
-	public void addVm(Vm vm) {
-		currentLoadPecentage = (double) vm.size / capacity * MAXIMUM_LOAD;
-	}
+    public void addVm(Vm vm) {
+        currentLoadPecentage = (double) vm.size / capacity * MAXIMUM_LOAD;
+    }
+
+    public int countVms() {
+        return 0;
+    }
 }
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmsCountMatcher.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by piotr on 23.06.16.
+ */
+public class ServerVmsCountMatcher extends TypeSafeMatcher<Server> {
+    private int expectedVmsCount;
+
+    public ServerVmsCountMatcher(int expectedVmsCount) {
+        this.expectedVmsCount = expectedVmsCount;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return expectedVmsCount==server.countVms();
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("a server with vms count of ").appendValue(expectedVmsCount);
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("a server with vms count of").appendValue(item.countVms());
+    }
+}
